ac19d02fa75c808ccd3f852153e5d6fe3d24bb6f
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2024-07-24 15:57:09 +0200

    WIP: Version command

--- grep.ts
+++ grep.ts
@@ -1,5 +1,17 @@
-import { editor } from "$sb/syscalls.ts";
+import { editor, shell } from "$sb/syscalls.ts";
+import { readSetting } from "$sb/lib/settings_page.ts";
 
-export async function helloWorld() {
-  await editor.flashNotification("Hello world!");
+const VERSION = "0.1.0";
+
+export async function showVersion() {
+  try {
+    const { stdout } = await shell.run("rg", ["--version"]);
+    const ripgrepVersion = stdout.split("\n")[0];
+    await editor.flashNotification(`Grep Plug ${VERSION}\n${ripgrepVersion}`);
+  } catch {
+    await editor.flashNotification(
+      "Could not run 'rg' command, make sure ripgrep is in PATH",
+      "error"
+    );
+  }
 }

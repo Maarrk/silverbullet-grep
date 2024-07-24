9f07b4e1826e8a567f3be4ca54ad8ecfa7735a2c
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2024-07-24 18:19:39 +0200

    Working search

--- grep.ts
+++ grep.ts
@@ -1,7 +1,8 @@
 import { editor, shell } from "$sb/syscalls.ts";
-import { readSetting } from "$sb/lib/settings_page.ts";
 
-const VERSION = "0.1.0";
+const VERSION = "0.2.0";
+
+const resultPage = "GREP RESULT";
 
 export async function showVersion() {
   try {
@@ -15,4 +16,113 @@ export async function showVersion() {
       "error"
     );
   }
+}
+
+async function grep(
+  pattern: string,
+  literal: boolean = false,
+  folder: string = "."
+) {
+  console.log(`grep("${pattern}", ${literal}, "${folder}")`);
+  let output: string;
+  try {
+    const result = await shell.run("rg", [
+      "--heading", // group by file
+      "--byte-offset", // location in file
+      literal ? "--fixed-strings" : "--regexp",
+      pattern,
+      folder,
+    ]);
+    if (result) {
+      output = result.stdout;
+    } else {
+      editor.flashNotification(`Pattern "${pattern}" produced no results`);
+      return;
+    }
+  } catch (err) {
+    console.error(err);
+    await editor.flashNotification(
+      "Error running 'rg' command, make sure ripgrep is in PATH",
+      "error"
+    );
+    return;
+  }
+
+  if (!output) {
+    editor.flashNotification(`Pattern "${pattern}" produced no results`);
+    return;
+  }
+
+  // by default ripgrep separates files by an empty line
+  const fileOutputs = output.split("\n\n");
+  console.log(fileOutputs);
+
+  const fileMatches = [];
+  for (const fileOutput of fileOutputs) {
+    const lines = fileOutput.split("\n");
+
+    // ensure it's a markdown file and normalize to page name
+    if (!lines[0].endsWith(".md")) continue;
+    const page = normalizePath(lines[0].slice(0, -3));
+
+    // don't consider hits in results
+    if (page === resultPage) continue;
+
+    const matches = [];
+    for (const line of lines.slice(1)) {
+      const locationMatch = line.match(/^(\d)+:/);
+      if (!locationMatch) continue;
+
+      // HACK: regex kept losing the first digit
+      const location = parseInt(line.split(":")[0]);
+      const context = line.substring(location.toString().length + 1);
+      matches.push({ location, context });
+    }
+    fileMatches.push({ page, matches });
+  }
+
+  fileMatches.sort((a, b) => {
+    // descending sort by match count
+    return -(a.matches.length - b.matches.length);
+  });
+
+  console.log(fileMatches);
+  const text = `#meta\n\n# Search results for "${pattern}"\n${fileMatches
+    .map(
+      (fm) =>
+        `* [[${fm.page}]] ${fm.matches.length} (${
+          fm.matches.length > 1 ? "matches" : "match"
+        }):\n` +
+        fm.matches
+          .map((m) => `  * [[${fm.page}@${m.location}]]: ${m.context}`)
+          .join("\n")
+    )
+    .join("\n")}
+    `;
+
+  await editor.navigate({ page: resultPage });
+  const textLength = (await editor.getText()).length;
+  await editor.replaceRange(0, textLength, text);
+}
+
+export async function searchText() {
+  const pattern = await editor.prompt("Literal text:", "");
+  if (!pattern) {
+    return;
+  }
+  await grep(pattern, true);
+}
+
+export async function searchRegex() {
+  const pattern = await editor.prompt("Regular expression pattern:", "");
+  if (!pattern) {
+    return;
+  }
+  await grep(pattern, false);
+}
+
+function normalizePath(path: string): string {
+  const forward = path.replaceAll("\\", "/");
+  if (forward.startsWith("./")) return forward.substring(2);
+  else return forward;
 }

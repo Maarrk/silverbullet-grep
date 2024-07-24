d5c7295dec7981eebd014e738cc445f2c093a03c
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2024-07-24 16:46:12 +0200

    Fix version command

--- grep.ts
+++ grep.ts
@@ -6,8 +6,9 @@ const VERSION = "0.1.0";
 export async function showVersion() {
   try {
     const { stdout } = await shell.run("rg", ["--version"]);
+    // Version info is in the first line, the rest are extensions
     const ripgrepVersion = stdout.split("\n")[0];
-    await editor.flashNotification(`Grep Plug ${VERSION}\n${ripgrepVersion}`);
+    await editor.flashNotification(`Grep Plug ${VERSION} ${ripgrepVersion}`);
   } catch {
     await editor.flashNotification(
       "Could not run 'rg' command, make sure ripgrep is in PATH",
